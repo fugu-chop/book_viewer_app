d3a7a69e4e13cf5b8d1cc3ffb4e4b6965d5522f4
Dean Wan <deanwan14@gmail.com>
2021-05-15 12:14:05 +1000

    Morning updates, 15th May

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,18 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
+# Tilt is an adapter between web applications and different Ruby templating languages.
+# It's like a USB plug: devices can communicate without having to know about each other when they are built.
 require "tilt/erubis"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  # Necessary because whitespace is ignored in HTML (i.e. paragraph breaks)
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+end
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb(:home)
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.readlines("data/chp1.txt")
+get "/chapters/:number" do
+  # Note this returns a string
+  number = params[:number].to_i
+  @title = "Chapter #{number}: #{@contents[number - 1]}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb(:chapter)
 end
