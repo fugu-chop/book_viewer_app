0427767df5e43fb60bb44461ad94de56c7c58228
Dean Wan <deanwan14@gmail.com>
2021-05-15 14:32:16 +1000

    Afternoon updates, 15th May

--- book_viewer.rb
+++ book_viewer.rb
@@ -11,9 +11,39 @@ end
 helpers do
   # Necessary because whitespace is ignored in HTML (i.e. paragraph breaks)
   def in_paragraphs(text)
-    text.split("\n\n").map do |paragraph|
-      "<p>#{paragraph}</p>"
+    text.split("\n\n").each_with_index.map do |line, index|
+      # Use jumplinks to focus on paragraph
+      "<p id=paragraph#{index}>#{line}</p>"
     end.join
+  end
+
+  # Grab a list of all the text
+  def each_chapter
+    @contents.each_with_index do |name, index|
+      number = index + 1
+      contents = File.read("data/chp#{number}.txt")
+      yield(number, name, contents)
+    end
+  end
+
+  # Check whether any of the text contains the search string
+  def chapters_matching(query)  
+    results = []
+    return results if !query || query.empty?
+
+    each_chapter do |number, name, contents|
+      matches = {}
+      contents.split("\n\n").each_with_index do |paragraph, index|
+        # Filtering occurs within the block
+        matches[index] = paragraph if paragraph.include?(query)
+      end
+      # The .any? call is equivalent to !matches.empty? 
+      # It's necessary to filter out empty hashes, since the each_with_index will iterate through each chapter
+      # When nothing matches in the chapter, the matches hash is empty and would otherwise make the results array
+      results << { number: number, name: name, paragraphs: matches } if matches.any?
+    end
+
+    results
   end
 end
 
@@ -26,8 +56,23 @@ end
 get "/chapters/:number" do
   # Note this returns a string
   number = params[:number].to_i
+
+  # Return true if number is between beginning and end of range
+  redirect "/" unless (1..@contents.size).cover?(number)
+
   @title = "Chapter #{number}: #{@contents[number - 1]}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb(:chapter)
+end
+
+# We're using GET as the method because performing a search doesn't modify any data.
+# If our form submission was modifying data, we would use POST as the form's method.
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb(:search)
+end
+
+not_found do
+  redirect "/"
 end
